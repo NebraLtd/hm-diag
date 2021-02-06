7f9c16a341517fa59cd783ac15f2feb40ce061c6
Ryan Walmsley <ryan@ryanteck.uk>
2021-02-06 16:51:15 +0000

    Start nginx

--- startDiag.sh
+++ startDiag.sh
@@ -3,7 +3,7 @@
 #Run diag
 rm /opt/nebraDiagnostics/html/index.html
 cp /opt/nebraDiagnostics/html/index.html.template /opt/nebraDiagnostics/html/index.html
-
+nginx
 sleep 90
 rm /opt/nebraDiagnostics/html/index.html
 python3 -u /opt/nebraDiagnostics/diagnosticsProgram.py
