472bfe2ff4860500330c40330b0b81afa0435aed
Ryan Walmsley <ryan@ryanteck.uk>
2021-02-07 16:52:55 +0000

    Shorten delay.

--- startDiag.sh
+++ startDiag.sh
@@ -4,6 +4,6 @@
 rm /opt/nebraDiagnostics/html/index.html
 cp /opt/nebraDiagnostics/html/index.html.template /opt/nebraDiagnostics/html/index.html
 nginx
-sleep 90
+sleep 30
 rm /opt/nebraDiagnostics/html/index.html
 python3 -u /opt/nebraDiagnostics/diagnosticsProgram.py
