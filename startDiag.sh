06fa1c44f2b62dcbe60a4c17dc52570f63631cca
Ryan Walmsley <ryan@ryanteck.uk>
2021-02-28 11:23:41 +0000

    Try to remove any delay.

--- startDiag.sh
+++ startDiag.sh
@@ -5,6 +5,6 @@ rm /opt/nebraDiagnostics/html/index.html
 rm /opt/nebraDiagnostics/html/initFile.txt
 cp /opt/nebraDiagnostics/html/index.html.template /opt/nebraDiagnostics/html/index.html
 nginx
-sleep 30
+#sleep 30
 rm /opt/nebraDiagnostics/html/index.html
 python3 -u /opt/nebraDiagnostics/diagnosticsProgram.py
