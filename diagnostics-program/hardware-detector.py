1c8d8665d2a42599fb4a280f81c1ce64cf3f3fd1
Ryan Walmsley <ryan@ryanteck.uk>
2021-05-10 20:48:32 +0100

    Started re-work of detecting hardware variants.

--- diagnostics-program/hardware-detector.py
+++ diagnostics-program/hardware-detector.py
@@ -6,52 +6,156 @@
 # and the network interface mac to use.
 
 variant_definitions = {
-# Nebra Indoor Hotspot
-"NEB-IN" : {
-    'spibus': 'spidev1.2',
-    'reset' : 38,
-    'mac' : 'eth0',
-    'fcc-id' : '2AZDM-HNTIN',
-    'ic-id': '27187-HNTIN'
-    },
-# Nebra Outdoor Hotspot
-"NEB-OUT" : {
-    'spibus': 'spidev1.2',
-    'reset' : 38,
-    'mac' : 'eth0',
-    'fcc-id' : '2AZDM-HNTOUT',
-    'ic-id': '27187-HNTOUT'
-    },
-# Nebra Light Pi Zero SPI Hotspot
-"NEB-LITE-PZW-SPI" : {
-    'spibus': 'spidev1.2',
-    'reset' : 22,
-    'mac' : 'wlan0',
-    'fcc-id' : '2AZDM-HNTPZI',
-    'ic-id': '27187-HNTPZI'
-    },
-# Nebra Light Pi Zero USB Hotspot
-"NEB-LITE-PZW-USB" : {
-    'spibus': 'spidev1.2',
-    'reset' : 22,
-    'mac' : 'wlan0',
-    'fcc-id' : '2AZDM-HNTPZX',
-    'ic-id': '27187-HNTPZX'
-    },
-# Nebra Light Pocket Beaglebone USB Hotspot
-"NEB-LITE-PBB" : {
-    'spibus': 'spidev0.0',
-    'reset' : 1,
-    'mac' : 'eth0',
-    'fcc-id' : '2AZDM-HNTPBU',
-    'ic-id': '27187-HNTPBU'
-    }
-# Original Helium Hotspot
-#"HELIUM-HOTSPOT" : {'spibus': 'spidev0.0', 'reset' : 42, 'mac' : 'wlan0'},
-# RAK Hotspot
-#"RAK-HOTSPOT" : {'spibus': 'spidev0.0', 'reset' : 42, 'mac' : 'wlan0'},
-# Syncrobit Hotspot
-#"SYNCROBIT" : {'spibus': 'spidev0.0', 'reset' : 42, 'mac' : 'wlan0'}
+    # Nebra Indoor Hotspot Gen1
+    "NEBHNT-IN1" : {
+        'spibus': 'spidev1.2',
+        'reset' : 38,
+        'mac' : 'eth0',
+        'status': 25,
+        'button': 26,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Outdoor Hotspot Gen1
+    "NEBHNT-OUT1" : {
+        'spibus': 'spidev1.2',
+        'reset' : 38,
+        'mac' : 'eth0',
+        'status': 25,
+        'button': 26,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Pi 0 Light Hotspot SPI Ethernet
+    "NEBHNT-LGT-ZS" : {
+        'spibus': 'spidev1.2',
+        'reset' : 22,
+        'mac' : 'wlan0',
+        'status': 24,
+        'button': 23,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Pi 0 Light Hotspot USB Ethernet
+    "NEBHNT-LGT-ZX" : {
+        'spibus': 'spidev1.2',
+        'reset' : 22,
+        'mac' : 'wlan0',
+        'status': 24,
+        'button': 23,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Beaglebone Light Hotspot
+    "NEBHNT-BBB" : {
+        'spibus': 'spidev1.0',
+        'reset' : 60,
+        'mac' : 'eth0',
+        'status': 31,
+        'button': 30,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Pocket Beagle Light Hotspot
+    "NEBHNT-PBB" : {
+        'spibus': 'spidev1.2',
+        'reset' : 60,
+        'mac' : 'wlan0',
+        'status': 31,
+        'button': 30,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Hotspot HAT Rockpi 4
+    "NEBHNT-HHRK4" : {
+        'spibus': 'spidev1.0',
+        'reset' : 149,
+        'mac' : 'eth0',
+        'status': 156,
+        'button': 154,
+        'ecc': True
+        },
+
+    # Nebra Hotspot HAT RPi 3/4 Full
+    "NEBHNT-HHRPI" : {
+        'spibus': 'spidev0.0',
+        'reset' : 22,
+        'mac' : 'eth0',
+        'status': 24,
+        'button': 23,
+        'ecc': True
+        },
+
+    # Nebra Hotspot HAT RPi Light
+    "NEBHNT-HHRPL" : {
+        'spibus': 'spidev0.0',
+        'reset' : 22,
+        'mac' : 'eth0',
+        'status': 24,
+        'button': 23,
+        'ecc': True
+        },
+
+    # Nebra Hotspot HAT Tinkerboard
+    "NEBHNT-HHTK" : {
+        'spibus': 'spidev1.2',
+        'reset' : 38,
+        'mac' : 'eth0',
+        'status': 25,
+        'button': 26,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Hotspot HAT Tinkerboard
+    "NEBHNT-HHTK2" : {
+        'spibus': 'spidev1.2',
+        'reset' : 38,
+        'mac' : 'eth0',
+        'status': 25,
+        'button': 26,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Indoor Hotspot
+    "NEBHNT-IN1" : {
+        'spibus': 'spidev1.2',
+        'reset' : 38,
+        'mac' : 'eth0',
+        'status': 25,
+        'button': 26,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Indoor Hotspot
+    "NEBHNT-IN1" : {
+        'spibus': 'spidev1.2',
+        'reset' : 38,
+        'mac' : 'eth0',
+        'status': 25,
+        'button': 26,
+        'ecc': True,
+        'type': "Full"
+        },
+
+    # Nebra Indoor Hotspot
+    "NEBHNT-IN1" : {
+        'spibus': 'spidev1.2',
+        'reset' : 38,
+        'mac' : 'eth0',
+        'status': 25,
+        'button': 26,
+        'ecc': True,
+        'type': "Full"
+        }
 }
 
 # List of USB Wi-Fi Adaptors used
