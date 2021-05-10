edc8197f01b4c71504e58d5871aeb5499ff7d2fd
Ryan Walmsley <ryan@ryanteck.uk>
2021-05-10 21:15:12 +0100

    Capitalises all the variables.

--- diagnostics-program/hardware_detector.py
+++ diagnostics-program/hardware_detector.py
@@ -2,170 +2,170 @@
 
 # Pin Definitions
 
-# Stores the SPI bus to be used, the reset pin,
-# and the network interface mac to use.
+# Stores the SPI bus to be used, the RESET pin,
+# and the network interface MAC to use.
 
 variant_definitions = {
     # Nebra Indoor Hotspot Gen1
     "NEBHNT-IN1" : {
-        'spibus': 'spidev1.2',
-        'reset' : 38,
-        'mac' : 'eth0',
-        'status': 25,
-        'button': 26,
-        'ecc': True,
-        'type': "Full"
+        'SPIBUS': 'spidev1.2',
+        'RESET' : 38,
+        'MAC' : 'eth0',
+        'STATUS': 25,
+        'BUTTON': 26,
+        'ECCOB': True,
+        'TYPE': "Full"
         },
 
     # Nebra Outdoor Hotspot Gen1
     "NEBHNT-OUT1" : {
-        'spibus': 'spidev1.2',
-        'reset' : 38,
-        'mac' : 'eth0',
-        'status': 25,
-        'button': 26,
-        'ecc': True,
-        'type': "Full"
+        'SPIBUS': 'spidev1.2',
+        'RESET' : 38,
+        'MAC' : 'eth0',
+        'STATUS': 25,
+        'BUTTON': 26,
+        'ECCOB': True,
+        'TYPE': "Full"
         },
 
     # Nebra Pi 0 Light Hotspot SPI Ethernet
     "NEBHNT-LGT-ZS" : {
-        'spibus': 'spidev1.2',
-        'reset' : 22,
-        'mac' : 'wlan0',
-        'status': 24,
-        'button': 23,
-        'ecc': True,
-        'type': "Full"
+        'SPIBUS': 'spidev1.2',
+        'RESET' : 22,
+        'MAC' : 'wlan0',
+        'STATUS': 24,
+        'BUTTON': 23,
+        'ECCOB': True,
+        'TYPE': "Full"
         },
 
     # Nebra Pi 0 Light Hotspot USB Ethernet
     "NEBHNT-LGT-ZX" : {
-        'spibus': 'spidev1.2',
-        'reset' : 22,
-        'mac' : 'wlan0',
-        'status': 24,
-        'button': 23,
-        'ecc': True,
-        'type': "Full"
+        'SPIBUS': 'spidev1.2',
+        'RESET' : 22,
+        'MAC' : 'wlan0',
+        'STATUS': 24,
+        'BUTTON': 23,
+        'ECCOB': True,
+        'TYPE': "Full"
         },
 
     # Nebra Beaglebone Light Hotspot
     "NEBHNT-BBB" : {
-        'spibus': 'spidev1.0',
-        'reset' : 60,
-        'mac' : 'eth0',
-        'status': 31,
-        'button': 30,
-        'ecc': True,
-        'type': "Full"
+        'SPIBUS': 'spidev1.0',
+        'RESET' : 60,
+        'MAC' : 'eth0',
+        'STATUS': 31,
+        'BUTTON': 30,
+        'ECCOB': True,
+        'TYPE': "Full"
         },
 
     # Nebra Pocket Beagle Light Hotspot
     "NEBHNT-PBB" : {
-        'spibus': 'spidev1.2',
-        'reset' : 60,
-        'mac' : 'wlan0',
-        'status': 31,
-        'button': 30,
-        'ecc': True,
-        'type': "Full"
+        'SPIBUS': 'spidev1.2',
+        'RESET' : 60,
+        'MAC' : 'wlan0',
+        'STATUS': 31,
+        'BUTTON': 30,
+        'ECCOB': True,
+        'TYPE': "Full"
         },
 
     # Nebra Hotspot HAT Rockpi 4
     "NEBHNT-HHRK4" : {
-        'spibus': 'spidev1.0',
-        'reset' : 149,
-        'mac' : 'eth0',
-        'status': 156,
-        'button': 154,
-        'ecc': True
+        'SPIBUS': 'spidev1.0',
+        'RESET' : 149,
+        'MAC' : 'eth0',
+        'STATUS': 156,
+        'BUTTON': 154,
+        'ECCOB': True
         },
 
     # Nebra Hotspot HAT RPi 3/4 Full
     "NEBHNT-HHRPI" : {
-        'spibus': 'spidev0.0',
-        'reset' : 22,
-        'mac' : 'eth0',
-        'status': 24,
-        'button': 23,
-        'ecc': True
+        'SPIBUS': 'spidev0.0',
+        'RESET' : 22,
+        'MAC' : 'eth0',
+        'STATUS': 24,
+        'BUTTON': 23,
+        'ECCOB': True
         },
 
     # Nebra Hotspot HAT RPi Light
     "NEBHNT-HHRPL" : {
-        'spibus': 'spidev0.0',
-        'reset' : 22,
-        'mac' : 'eth0',
-        'status': 24,
-        'button': 23,
-        'ecc': True
+        'SPIBUS': 'spidev0.0',
+        'RESET' : 22,
+        'MAC' : 'eth0',
+        'STATUS': 24,
+        'BUTTON': 23,
+        'ECCOB': True
         },
 
     # Nebra Hotspot HAT Tinkerboard 1
     "NEBHNT-HHTK" : {
-        'spibus': 'spidev2.0',
-        'reset' : 167,
-        'mac' : 'eth0',
-        'status': 163,
-        'button': 162,
-        'ecc': True,
-        'type': "Light"
+        'SPIBUS': 'spidev2.0',
+        'RESET' : 167,
+        'MAC' : 'eth0',
+        'STATUS': 163,
+        'BUTTON': 162,
+        'ECCOB': True,
+        'TYPE': "Light"
         },
 
     # Nebra Hotspot HAT Tinkerboard 2
     "NEBHNT-HHTK2" : {
-        'spibus': 'spidev2.0',
-        'reset' : 167,
-        'mac' : 'eth0',
-        'status': 163,
-        'button': 162,
-        'ecc': True,
-        'type': "Full"
+        'SPIBUS': 'spidev2.0',
+        'RESET' : 167,
+        'MAC' : 'eth0',
+        'STATUS': 163,
+        'BUTTON': 162,
+        'ECCOB': True,
+        'TYPE': "Full"
         },
 
     # RAKwireless Hotspot Miner
     "COMP-RAKHM" : {
-        'spibus': 'spidev0.0',
-        'reset' : 17,
-        'mac' : 'wlan0',
-        'status': 20,
-        'button': 21,
-        'ecc': True,
-        'type': "Full"
+        'SPIBUS': 'spidev0.0',
+        'RESET' : 17,
+        'MAC' : 'wlan0',
+        'STATUS': 20,
+        'BUTTON': 21,
+        'ECCOB': True,
+        'TYPE': "Full"
         },
 
     # Helium Hotspot
     "COMP-HELIUM" : {
-        'spibus': 'spidev0.0',
-        'reset' : 17,
-        'mac' : 'wlan0',
-        'status': 20,
-        'button': 21,
-        'ecc': True,
-        'type': "Full"
+        'SPIBUS': 'spidev0.0',
+        'RESET' : 17,
+        'MAC' : 'wlan0',
+        'STATUS': 20,
+        'BUTTON': 21,
+        'ECCOB': True,
+        'TYPE': "Full"
         },
 
     # DIY Pi Supply Hotspot HAT
     "DIY-PISLGH" : {
-        'spibus': 'spidev0.0',
-        'reset' : 22,
-        'mac' : 'eth0',
-        'status': 20,
-        'button': 21,
-        'ecc': False,
-        'type': "Light"
+        'SPIBUS': 'spidev0.0',
+        'RESET' : 22,
+        'MAC' : 'eth0',
+        'STATUS': 20,
+        'BUTTON': 21,
+        'ECCOB': False,
+        'TYPE': "Light"
         },
 
     # Nebra Indoor Hotspot
     "DIY-RAK2287" : {
-        'spibus': 'spidev0.0',
-        'reset' : 17,
-        'mac' : 'eth0',
-        'status': 20,
-        'button': 21,
-        'ecc': False,
-        'type': "Light"
+        'SPIBUS': 'spidev0.0',
+        'RESET' : 17,
+        'MAC' : 'eth0',
+        'STATUS': 20,
+        'BUTTON': 21,
+        'ECCOB': False,
+        'TYPE': "Light"
         }
 }
 
