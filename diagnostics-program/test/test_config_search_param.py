e3f4eb1627d22b78c022fafd26a310fa28fe0083
Ilya Starodubtsev <ilyastrodubtsev@gmail.com>
2021-06-22 15:57:44 +0600

    add unittests

---
+++ diagnostics-program/test/test_config_search_param.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import patch
+import subprocess
+import sys
+sys.path.append("..")
+from main import config_search_param # noqa
+
+
+class TestConfigSearch(unittest.TestCase):
+    @patch('subprocess.Popen')
+    def test_correct_param(self, null):
+        subprocess.Popen.return_value = "60--"
+        result = config_search_param("somecommand", "60--")
+        self.assertEqual(result, True)
+
+    @patch('subprocess.Popen')
+    def test_incorrect_param(self, null):
+        subprocess.Popen.return_value = "some param lister"
+        result = config_search_param("somecommand", "60--")
+        self.assertEqual(result, False)
+
+    def test_types(self):
+        self.assertRaises(TypeError, config_search_param, 1, 2)
+        self.assertRaises(TypeError, config_search_param, "123321", 1)
+        self.assertRaises(TypeError, config_search_param, 1, "123321")
