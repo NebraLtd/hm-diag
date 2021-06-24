2b57860a1212d65fc17ff64c56a8b33530546031
Ilya Starodubtsev <ilyastrodubtsev@gmail.com>
2021-06-24 14:20:57 +0600

    remove commented strings in startDiag script

--- diagnostics-program/startDiag.sh
+++ diagnostics-program/startDiag.sh
@@ -3,10 +3,6 @@
 #Run diag
 rm -f /opt/nebraDiagnostics/html/index.html
 rm -f /opt/nebraDiagnostics/html/initFile.txt
-#rm -f /var/data/public_keys
-#cp /opt/nebraDiagnostics/html/index.html.template /opt/nebraDiagnostics/html/index.html
 
 nginx
-#sleep 30
-#rm -f /opt/nebraDiagnostics/html/index.html
 python3 -u /opt/nebraDiagnostics/main.py
