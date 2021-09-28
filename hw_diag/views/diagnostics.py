15ede01a94fd5441bb25974bdc00e228e46c6115
Viktor Petersson <self@vpetersson.com>
2021-09-28 11:28:59 +0100

    Moves function to hm-pyhelper

--- hw_diag/views/diagnostics.py
+++ hw_diag/views/diagnostics.py
@@ -8,8 +8,8 @@ from flask import jsonify
 
 from hm_pyhelper.miner_param import get_ethernet_addresses
 from hm_pyhelper.miner_param import get_public_keys_rust
+from hm_pyhelper.miner_param import get_gateway_mfr_test_result
 from hw_diag.utilities.hardware import should_display_lte
-from hw_diag.utilities.miner import get_gateway_mfr_test_result
 from hw_diag.utilities.hardware import get_rpi_serial
 from hw_diag.utilities.hardware import lora_module_test
 from hw_diag.utilities.hardware import set_diagnostics_bt_lte
