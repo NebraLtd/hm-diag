9ae928441be37d2c8ac2c82f3ade9e88721aac18
Viktor Petersson <vpetersson@users.noreply.github.com>
2021-10-15 11:22:23 +0100

    Brings in new features from hm-pyhelper (#162)
    * Introduces key provisioning for hm-diag (which makes hm-gwmfr redundant)
    * Tweaks MAC address parsing

--- hw_diag/app.py
+++ hw_diag/app.py
@@ -3,9 +3,11 @@ import os
 
 from flask import Flask
 from flask_apscheduler import APScheduler
+from retry import retry
 
 from hw_diag.tasks import perform_hw_diagnostics
 from hw_diag.views.diagnostics import DIAGNOSTICS
+from hm_pyhelper.miner_param import provision_key
 
 
 DEBUG = os.getenv('DEBUG', 0)
@@ -15,6 +17,12 @@ log = logging.getLogger()
 if DEBUG:
     # Defaults to INFO if not explicitly set.
     log.setLevel(logging.DEBUG)
+
+
+@retry(ValueError, tries=10, delay=1, backoff=2, logger=log)
+def perform_key_provisioning():
+    if not provision_key():
+        raise ValueError
 
 
 def get_app(name):
@@ -47,6 +55,12 @@ def get_app(name):
 
 
 def main():
+
+    """
+    Let's make sure we provision the key first.
+    """
+    perform_key_provisioning()
+
     app = get_app(__name__)
     debug = bool(DEBUG)
     app.run('0.0.0.0', threaded=True, debug=debug)
