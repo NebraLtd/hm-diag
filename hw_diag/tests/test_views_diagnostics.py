445ec9f1bdd1e066f0702aafc0ac85ba8a7c361a
Ilya Starodubtsev <ilyastrodubtsev@gmail.com>
2021-10-21 16:27:58 +0600

    fix unittest

--- hw_diag/tests/test_views_diagnostics.py
+++ hw_diag/tests/test_views_diagnostics.py
@@ -1,6 +1,7 @@
 import unittest
 import flask
 from werkzeug.datastructures import ImmutableMultiDict
+from unittest.mock import patch, mock_open
 
 
 # Test cases
@@ -8,6 +9,9 @@ from hw_diag.app import get_app
 
 
 class TestGetDiagnostics(unittest.TestCase):
+    TEST_DATA = """Revision        : a020d3
+    Serial\t\t: 00000000a3e7kg80
+    Model           : Raspberry Pi 3 Model B Plus Rev 1.3 """
 
     def setUp(self):
         self.app = get_app('test_app')
@@ -27,7 +31,6 @@ class TestGetDiagnostics(unittest.TestCase):
     def test_get_json_output(self):
         # Check the diagnostics JSON output.
         url = '/?json=true'
-
         # Server perspective
         with self.app.test_request_context(url):
             resp = flask.Response({})
@@ -41,18 +44,23 @@ class TestGetDiagnostics(unittest.TestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertEqual(resp.status, '200 OK')
 
-        # Client perspective
-        with self.app.test_client() as c:
-            cresp = c.get(url)
-            error_str = ('Diagnostics have not yet run, '
-                         'please try again in a few minutes'
-                         )
-            expected = {'error': error_str}
-            self.assertEqual(cresp.json, expected)
-            self.assertEqual(
-                cresp.headers.get('Content-Type'),
-                'application/json'
-            )
+            # Client perspective
+            m = mock_open(read_data='/proc/cpuinfo'.join(self.TEST_DATA))
+            with patch('builtins.open', m):
+                s = mock_open(read_data='diagnostic_data.json')
+                with patch('builtins.open', s):
+                    s.side_effect = FileNotFoundError
+                    with self.app.test_client() as c:
+                        cresp = c.get(url)
+                        error_str = ('Diagnostics have not yet run, '
+                                     'please try again in a few minutes'
+                                     )
+                        expected = {'error': error_str}
+                        self.assertEqual(cresp.json, expected)
+                        self.assertEqual(
+                            cresp.headers.get('Content-Type'),
+                            'application/json'
+                        )
 
     def test_initFile_output(self):
         # Check the diagnostics JSON output.
