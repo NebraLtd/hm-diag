6b174ec952d4b586a0fc000cac256ade3297443f
Viktor Petersson <self@vpetersson.com>
2021-10-22 09:44:39 +0100

    Dont read key from miner

--- hw_diag/utilities/miner.py
+++ hw_diag/utilities/miner.py
@@ -5,19 +5,13 @@ client = MinerClient()
 
 
 def fetch_miner_data(diagnostics):
-    # Fetch miner keys from miner container and append
-    # them to the diagnostics dictionary.
     try:
-        public_key = client.get_peer_addr().get('peer_addr').split('/')[2]
         peerbook = client.get_peer_book()[0]
         height = client.get_height()
     except Exception as err:
-        raise Exception("Unable to fetch keys from miner container "
+        raise Exception("Unable to fetch keys from miner container"
                         "via JSON RPC API. Exception: %s" % str(err))
 
-    diagnostics['PK'] = public_key
-    diagnostics['OK'] = public_key
-    diagnostics['AN'] = peerbook.get('name')
     diagnostics['MC'] = peerbook.get('connection_count') > 1
     diagnostics['MD'] = peerbook.get('listen_addr_count') > 0
     diagnostics['MH'] = height.get('height')
