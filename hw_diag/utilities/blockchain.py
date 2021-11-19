7e26f354a01476273b03766fe33b16dc92d943fa
Sebastian Maj <WolfTech@cogeco.ca>
2021-11-19 12:34:27 -0500

    fix: Helium API not returning (#236)
    * fix: Added headers to get BCH api call
    
    * fix: wrong formatting on string
    
    * refactor: update to modern user-agent string

--- hw_diag/utilities/blockchain.py
+++ hw_diag/utilities/blockchain.py
@@ -15,7 +15,14 @@ def get_helium_blockchain_height():
     Possible exceptions:
     TypeError - if the key ['data']['height'] in response is not found.
     """
+    headers = {
+                "user-agent": ("Mozilla/5.0"
+                               " (Macintosh; Intel Mac OS X 10_15_6)"
+                               " AppleWebKit/605.1.15 (KHTML, like Gecko)"
+                               " Version/14.1.1 Safari/605.1.15"),
+              }
     result = requests.get(HELIUM_MINER_HEIGHT_URL,
+                          headers=headers,
                           timeout=os.getenv('HELIUM_API_TIMEOUT_SECONDS', 5))
     if result.status_code == 200:
         result = result.json()
