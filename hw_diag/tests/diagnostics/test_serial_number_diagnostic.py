e370b3cf73e3661811b7485e7d4dff318154ddbc
Marvin Arnold <marvin@geeky.rocks>
2021-11-25 12:55:44 -0600

    fix: strip \x00 from serial number

--- hw_diag/tests/diagnostics/test_serial_number_diagnostic.py
+++ hw_diag/tests/diagnostics/test_serial_number_diagnostic.py
@@ -6,12 +6,26 @@ from hm_pyhelper.diagnostics.diagnostics_report import \
 from hw_diag.diagnostics.serial_number_diagnostic import SerialNumberDiagnostic
 
 VALID_CPU_PROC = """00000000ddd1a4c2"""
+PADDED_CPU_PROC = "%s\x00" % VALID_CPU_PROC
 
 
 class TestSerialNumberDiagnostic(unittest.TestCase):
 
     @patch("builtins.open", new_callable=mock_open, read_data=VALID_CPU_PROC)
     def test_success(self, mock):
+        diagnostic = SerialNumberDiagnostic()
+        diagnostics_report = DiagnosticsReport([diagnostic])
+        diagnostics_report.perform_diagnostics()
+
+        self.assertDictEqual(diagnostics_report, {
+            DIAGNOSTICS_PASSED_KEY: True,
+            DIAGNOSTICS_ERRORS_KEY: [],
+            'serial_number': '00000000ddd1a4c2',
+            'serial_number': '00000000ddd1a4c2'
+        })
+
+    @patch("builtins.open", new_callable=mock_open, read_data=PADDED_CPU_PROC)
+    def test_success_strip(self, mock):
         diagnostic = SerialNumberDiagnostic()
         diagnostics_report = DiagnosticsReport([diagnostic])
         diagnostics_report.perform_diagnostics()
--- hw_diag/tests/test_get_serial_number.py
+++ hw_diag/tests/test_get_serial_number.py
@@ -5,15 +5,22 @@ sys.path.append("..")
 from hw_diag.utilities.hardware import get_serial_number  # noqa
 
 
-class TestGetPublicKeys(unittest.TestCase):
+class TestGetSerialNumber(unittest.TestCase):
 
-    TEST_DATA = """00000000a3e7kg80"""
+    TEST_DATA = "00000000a3e7kg80"
 
     right_value = {'serial_number': '00000000a3e7kg80'}
     diag = {}
 
     def test_get_serialnumber(self):
-        m = mock_open(read_data=''.join(self.TEST_DATA))
+        m = mock_open(read_data=self.TEST_DATA)
+        with patch('builtins.open', m):
+            get_serial_number(self.diag)
+            self.assertEqual(self.diag["serial_number"],
+                             self.right_value["serial_number"])
+
+    def test_strip_serialnumber(self):
+        m = mock_open(read_data="%s\x00" % self.TEST_DATA)
         with patch('builtins.open', m):
             get_serial_number(self.diag)
             self.assertEqual(self.diag["serial_number"],
