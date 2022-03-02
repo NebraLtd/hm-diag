df8cce483d23be7fe54e617add5f17f3b3df4efa
Pritam Ghanghas <pghanghas@nebra.com>
2022-03-02 21:34:33 +0530

    feat: add EG25G firmware and flashing tool (#133)
    * mostly black formatting fixes.
    * some review comments.

--- hw_diag/tests/test_download.py
+++ hw_diag/tests/test_download.py
@@ -2,107 +2,115 @@ import unittest
 from unittest.mock import patch
 import os
 import responses
+import tempfile
 
 from hw_diag.utilities.download import validate_file, download_with_resume
 
-TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'data/nebra.webp')
-TESTDATA_SHA256 = 'aa3fbe2ddc4ea2ced20ce5d5a34b0f4b618a22bc0656d48a8be2166202d3a00c'
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), "data/nebra.webp")
+TESTDATA_SHA256 = "aa3fbe2ddc4ea2ced20ce5d5a34b0f4b618a22bc0656d48a8be2166202d3a00c"
 
 TEST_BLOCK_SIZE = 1024
 
-OUTPUT_FILENAME = '/tmp/nebra.webp'
+OUTPUT_FILENAME = "nebra.webp"
+
+MOCK_DOWNLOAD_URL = "https://example.com/test.webp"
+MOCK_DOWNLOAD_CONTENT_TYPE = "image/webp"
 
 
 def get_test_data():
     """
     Return the test data for download_with_resume
     """
-    with open(TESTDATA_FILENAME, 'rb') as f:
+    with open(TESTDATA_FILENAME, "rb") as f:
         data = f.read()
         return data
 
 
 def add_head_response(data):
-    responses.add(responses.HEAD, 'http://example.com/test.webp',
-                  status=200,
-                  content_type='image/webp',
-                  headers={'Content-Length': f'{len(data)}'})
+    responses.add(
+        responses.HEAD,
+        MOCK_DOWNLOAD_URL,
+        status=200,
+        content_type=MOCK_DOWNLOAD_CONTENT_TYPE,
+        headers={"Content-Length": f"{len(data)}"},
+    )
 
 
 def add_file_chunk_response(data, start, end):
     data_len = len(data)
-    responses.add(responses.GET, 'http://example.com/test.webp',
-                  body=data[start:end],
-                  status=200,
-                  content_type='image/webp',
-                  headers={'Content-Length': f'{data_len}',
-                               'Content-Range': f'bytes {start}-{end}/{data_len}'})
+    responses.add(
+        responses.GET,
+        MOCK_DOWNLOAD_URL,
+        body=data[start:end],
+        status=200,
+        content_type=MOCK_DOWNLOAD_CONTENT_TYPE,
+        headers={
+            "Content-Length": f"{data_len}",
+            "Content-Range": f"bytes {start}-{end}/{data_len}",
+        },
+    )
 
 
 def add_not_found_response():
-    responses.add(responses.GET, 'http://example.com/test.webp',
-                  status=404,
-                  content_type='image/webp')
+    responses.add(
+        responses.GET,
+        MOCK_DOWNLOAD_URL,
+        status=404,
+        content_type=MOCK_DOWNLOAD_CONTENT_TYPE,
+    )
 
 
 class TestDownload(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.cleanup()
-        return super().setUp()
-
-    def cleanup(self) -> None:
-        for filename in [OUTPUT_FILENAME, OUTPUT_FILENAME + '.part']:
-            if os.path.exists(filename):
-                os.remove(filename)
-
-        responses.reset()
-
     # @patch(requests.get, read_stream)
-    @patch('hw_diag.utilities.download.DEFAULT_BLOCK_SIZE', 1024)
+    @patch("hw_diag.utilities.download.DEFAULT_BLOCK_SIZE", 1024)
     @responses.activate
     def test_download(self):
-        test_data = get_test_data()
-        data_len = len(test_data)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            test_data = get_test_data()
+            data_len = len(test_data)
+            tmpfilename = os.path.join(tmpdir, OUTPUT_FILENAME)
 
-        # add responses in the order they will be required.
-        # header
-        add_head_response(test_data)
+            # add responses in the order they will be required.
+            # header
+            add_head_response(test_data)
 
-        # partial file
-        add_file_chunk_response(test_data, 0, 1024)
+            # partial file
+            add_file_chunk_response(test_data, 0, 1024)
 
-        # this will download part file as only 1024 bytes will be served.
-        download_with_resume('http://example.com/test.webp',
-                             OUTPUT_FILENAME, TESTDATA_SHA256)
-        part_filename = f'{OUTPUT_FILENAME}.part'
-        self.assertEqual(os.path.exists(part_filename), True)
-        self.assertEqual(validate_file(part_filename, TESTDATA_SHA256), False)
+            # this will download part file as only 1024 bytes will be served.
+            download_with_resume(MOCK_DOWNLOAD_URL, tmpfilename, TESTDATA_SHA256)
+            part_filename = f"{tmpfilename}.part"
+            self.assertEqual(os.path.exists(part_filename), True)
+            self.assertEqual(validate_file(part_filename, TESTDATA_SHA256), False)
 
-        # prepare for next test
-        add_head_response(test_data)
-        add_file_chunk_response(test_data, 1024, data_len)
+            # prepare for next test that completes the download
+            add_head_response(test_data)
+            add_file_chunk_response(test_data, 1024, data_len)
 
-        # this will complete the file
-        download_with_resume('http://example.com/test.webp',
-                             OUTPUT_FILENAME, TESTDATA_SHA256)
-        self.assertEqual(os.path.exists(part_filename), False)
-        self.assertEqual(os.path.exists(OUTPUT_FILENAME), True)
-        self.assertEqual(validate_file(OUTPUT_FILENAME, hash=TESTDATA_SHA256), True)
+            # this will complete the file
+            download_with_resume(MOCK_DOWNLOAD_URL, tmpfilename, TESTDATA_SHA256)
+            self.assertEqual(os.path.exists(part_filename), False)
+            self.assertEqual(os.path.exists(tmpfilename), True)
+            self.assertEqual(validate_file(tmpfilename, hash=TESTDATA_SHA256), True)
 
-        # cleanup
-        self.cleanup()
-        add_head_response(test_data)
-        add_not_found_response()
+    @patch("hw_diag.utilities.download.DEFAULT_BLOCK_SIZE", 1024)
+    @responses.activate
+    def test_file_not_found(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            test_data = get_test_data()
+            tmpfilename = os.path.join(tmpdir, OUTPUT_FILENAME)
 
-        self.assertEqual(os.path.exists(part_filename), False)
-        self.assertEqual(os.path.exists(OUTPUT_FILENAME), False)
-        download_with_resume('http://example.com/test.webp',
-                             OUTPUT_FILENAME, TESTDATA_SHA256)
-        self.assertEqual(os.path.exists(OUTPUT_FILENAME), False)
+            # test for file not found
+            add_head_response(test_data)
+            add_not_found_response()
+
+            self.assertEqual(os.path.exists(tmpfilename), False)
+            download_with_resume(MOCK_DOWNLOAD_URL, tmpfilename, TESTDATA_SHA256)
+            self.assertEqual(os.path.exists(tmpfilename), False)
 
     def test_validate_file(self):
-        block_variable_name = 'hw_diag.utilities.download.DEFAULT_BLOCK_SIZE'
+        block_variable_name = "hw_diag.utilities.download.DEFAULT_BLOCK_SIZE"
         with patch(block_variable_name, 1024) as _:
             self.assertTrue(validate_file(file_path=TESTDATA_FILENAME, hash=TESTDATA_SHA256))
         with patch(block_variable_name, 32) as _:
--- hw_diag/tests/test_quectel.py
+++ hw_diag/tests/test_quectel.py
@@ -12,14 +12,13 @@ from hw_diag.utilities.dbus_proxy.sim import Sim
 from hw_diag.utilities.keystore import KeyStore
 
 
-MOCKED_STATE_FILE = '/tmp/fw_state'
-MOCKED_MODEM0_PROPERTIES_GOOD1 = {'Revision': 'EG25GGBR07A08M2G'}
-MOCKED_MODEM0_PROPERTIES_GOOD2 = {'Revision': 'EG25GGBR07A07M2G'}
-MOCKED_MODEM0_PROPERTIES_BAD = {'Revision': 'EG25GGBR07A07M2H'}
+MOCKED_STATE_FILE = "./tmp_fw_state"
+MOCKED_MODEM0_PROPERTIES_GOOD1 = {"Revision": "EG25GGBR07A08M2G"}
+MOCKED_MODEM0_PROPERTIES_GOOD2 = {"Revision": "EG25GGBR07A07M2G"}
+MOCKED_MODEM0_PROPERTIES_BAD = {"Revision": "EG25GGBR07A07M2H"}
 
 
 class TestQuectel(unittest.TestCase):
-
     def setUp(self) -> None:
         if os.path.exists(MOCKED_STATE_FILE):
             os.remove(MOCKED_STATE_FILE)
@@ -36,10 +35,12 @@ class TestQuectel(unittest.TestCase):
 
         mocked_object.GetAll.return_value = modem_properties
 
-    @patch('dbus.SystemBus')
-    @patch('dbus.Interface')
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
     def test_find_modem_good(self, mock_interface, _):
-        # various revision for mocked modem, two known good for eg25g and one unknown
+        """
+        test two known good revisions for eg25g to test modem discovery function
+        """
         self.mock_modem(mock_interface, MOCKED_MODEM0_PROPERTIES_GOOD1)
         modem = find_eg25g_modem()
         self.assertIsInstance(modem, Modem)
@@ -48,95 +49,122 @@ class TestQuectel(unittest.TestCase):
         modem = find_eg25g_modem()
         self.assertIsInstance(modem, Modem)
 
-    @patch('dbus.SystemBus')
-    @patch('dbus.Interface')
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
     def test_find_modem_bad(self, mock_interface, _):
+        """
+        test that a unsupported modem identifier will result in no modem
+        found and failed upgrade.
+        """
         self.mock_modem(mock_interface, MOCKED_MODEM0_PROPERTIES_BAD)
         modem = find_eg25g_modem()
         self.assertIsNone(modem)
         self.assertFalse(firmware_upgrade_with_rollback())
-        self.assertFalse(update_setting_with_rollback('get_ue_mode', 'set_ue_mode', '01'))
+        self.assertFalse(update_setting_with_rollback("get_ue_mode", "set_ue_mode", "01"))
 
-    @patch('dbus.SystemBus')
-    @patch('dbus.Interface')
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
     def test_sim_identification(self, mock_interface, _):
+        """
+        Test if SIM identification works correctly.
+        """
         self.mock_modem(mock_interface, MOCKED_MODEM0_PROPERTIES_GOOD1)
 
         # create sim object
-        sim = Sim('/org.freedesktop.ModemManager1/Modem/0/Sim/0')
-        mock_interface.Get('Sim').return_value = sim
+        sim = Sim("/org.freedesktop.ModemManager1/Modem/0/Sim/0")
+        mock_interface.Get("Sim").return_value = sim
 
         # first return ATT operator id and then T-Mobile operator id
-        with patch('hw_diag.utilities.dbus_proxy.sim.Sim.get_property',
-                   side_effect=['310280', '310160']):
+        with patch(
+            "hw_diag.utilities.dbus_proxy.sim.Sim.get_property",
+            side_effect=["310280", "310160"],
+        ):
             self.assertTrue(is_att_sim())
             self.assertFalse(is_att_sim())
 
-    @patch('hw_diag.utilities.quectel.MODEM_RESET_WAIT_TIME', 0.01)
-    @patch('hw_diag.utilities.quectel.INTERNET_MAX_WAIT_TIME', 0.01)
-    @patch('hw_diag.utilities.quectel.FW_STATE_FILE', MOCKED_STATE_FILE)
-    @patch('dbus.SystemBus')
-    @patch('dbus.Interface')
-    @patch('hw_diag.utilities.dbus_proxy.systemd_unit.SystemDUnit._wait_state', return_value=True)
+    @patch("hw_diag.utilities.quectel.MODEM_RESET_WAIT_TIME", 0.001)
+    @patch("hw_diag.utilities.quectel.INTERNET_MAX_WAIT_TIME", 0.001)
+    @patch("hw_diag.utilities.quectel.FW_STATE_FILE", MOCKED_STATE_FILE)
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
+    @patch(
+        "hw_diag.utilities.dbus_proxy.systemd_unit.SystemDUnit._wait_state",
+        return_value=True,
+    )
     def test_firmware_upgrade_with_rollback(self, _, mock_interface, _2):
+        """
+        Test firmware upgrade with rollback function for external process failure
+        and rollback due to internet connectivity lost.
+        dbus messages about quectel modem are mocked
+        reset wait times are mocked to reduce test time, modem is mocked and there is
+        no real reset happening
+        Firmware_state filename is mocked so that we can independently test it.
+        """
+        mode_get_property_mock_id = "hw_diag.utilities.dbus_proxy.modem.Modem.get_property"
         self.mock_modem(mock_interface, MOCKED_MODEM0_PROPERTIES_GOOD1)
         # two modems for which we support firmware upgrade and one with revision
         # that we are not aware of.
-        with patch('hw_diag.utilities.quectel._do_upgrade', return_value=True):
-            with patch('hw_diag.utilities.dbus_proxy.modem.Modem.get_property',
-                       return_value='EG25GGBR07A08M2G'):
+        with patch("hw_diag.utilities.quectel._do_upgrade", return_value=True):
+            with patch(mode_get_property_mock_id, return_value="EG25GGBR07A08M2G"):
                 self.assertTrue(firmware_upgrade_with_rollback())
-            with patch('hw_diag.utilities.dbus_proxy.modem.Modem.get_property',
-                       return_value='EG25GGBR07A07M2G'):
+            with patch(mode_get_property_mock_id, return_value="EG25GGBR07A07M2G"):
                 self.assertTrue(firmware_upgrade_with_rollback())
-            with patch('hw_diag.utilities.dbus_proxy.modem.Modem.get_property',
-                       return_value='unknown'):
+            with patch(mode_get_property_mock_id, return_value="unknown"):
                 self.assertFalse(firmware_upgrade_with_rollback())
 
             # internet available before upgrade but lost after that.
-            with patch('hw_diag.utilities.quectel.is_internet_accessible',
-                       side_effect=[True, False]):
-                with patch('hw_diag.utilities.dbus_proxy.modem.Modem.get_property',
-                           return_value='EG25GGBR07A07M2G'):
+            with patch(
+                "hw_diag.utilities.quectel.is_internet_accessible",
+                side_effect=[True, False],
+            ):
+                with patch(mode_get_property_mock_id, return_value="EG25GGBR07A07M2G"):
                     self.assertTrue(firmware_upgrade_with_rollback())
-                    value = KeyStore(MOCKED_STATE_FILE).get(EG25G_DESIRED_FW['EG25GGBR07A07M2G'], 0)
+                    value = KeyStore(MOCKED_STATE_FILE).get(EG25G_DESIRED_FW["EG25GGBR07A07M2G"], 0)
                     self.assertEqual(value, 1)
 
         # subprocess failure
-        with patch('subprocess.check_output',
-                   side_effect=CalledProcessError(returncode=1, cmd='QFirehose',
-                                                  output='mocked error')):
-            modem_revision = 'EG25GGBR07A08M2G'
-            with patch('hw_diag.utilities.dbus_proxy.modem.Modem.get_property',
-                       return_value=modem_revision):
+        with patch(
+            "subprocess.check_output",
+            side_effect=CalledProcessError(returncode=1, cmd="QFirehose", output="mocked error"),
+        ):
+            modem_revision = "EG25GGBR07A08M2G"
+            with patch(mode_get_property_mock_id, return_value=modem_revision):
                 self.assertFalse(firmware_upgrade_with_rollback())
 
-    # this function only tests that the evn variable is working as expected.
-    @patch('hw_diag.utilities.quectel.ensure_modem_manager_health')
-    @patch('hw_diag.utilities.quectel.download_modem_firmware')
-    @patch('hw_diag.utilities.quectel.update_setting_with_rollback')
-    @patch('hw_diag.utilities.quectel.firmware_upgrade_with_rollback')
-    @patch('hw_diag.utilities.quectel.is_att_sim', return_value=True)
-    @patch('hw_diag.utilities.quectel.get_firmware_versions',
-           return_value=MOCKED_MODEM0_PROPERTIES_GOOD1['Revision'])
-    def test_evn_variable(self, mock_get_firmware_versions, mock_is_att_sim,
-                          mock_firmware_upgrade_with_rollback,
-                          mock_update_setting_with_rollback,
-                          mock_download_modem_firmware,
-                          mock_ensure_modem_manager_health,
-                          ):
+    @patch("hw_diag.utilities.quectel.ensure_modem_manager_health")
+    @patch("hw_diag.utilities.quectel.download_modem_firmware")
+    @patch("hw_diag.utilities.quectel.update_setting_with_rollback")
+    @patch("hw_diag.utilities.quectel.firmware_upgrade_with_rollback")
+    @patch("hw_diag.utilities.quectel.is_att_sim", return_value=True)
+    @patch(
+        "hw_diag.utilities.quectel.get_firmware_versions",
+        return_value=MOCKED_MODEM0_PROPERTIES_GOOD1["Revision"],
+    )
+    def test_evn_variable(
+        self,
+        mock_get_firmware_versions,
+        mock_is_att_sim,
+        mock_firmware_upgrade_with_rollback,
+        mock_update_setting_with_rollback,
+        mock_download_modem_firmware,
+        mock_ensure_modem_manager_health,
+    ):
+        """
+        tests side effects of env variable.
+        checks that right functions have been called with right count
+        """
 
         # if control variable is not defined, we should only download the firmware
         ensure_quectel_health()
         self.assertEqual(mock_ensure_modem_manager_health.call_count, 1)
         self.assertEqual(mock_download_modem_firmware.call_count, 1)
-        self.assertEqual(mock_update_setting_with_rollback.call_count, 0)
+        self.assertEqual(mock_update_setting_with_rollback.call_count, 2)
         self.assertEqual(mock_firmware_upgrade_with_rollback.call_count, 0)
 
         # if quectel is defined, we should run through all the steps
-        with patch.dict(os.environ, {'UPDATE_QUECTEL_EG25G_MODEM': '1'}):
+        with patch.dict(os.environ, {"UPDATE_QUECTEL_EG25G_MODEM": "1"}):
             ensure_quectel_health()
             self.assertEqual(mock_ensure_modem_manager_health.call_count, 2)
             self.assertEqual(mock_download_modem_firmware.call_count, 2)
-            self.assertEqual(mock_update_setting_with_rollback.call_count, 2)
+            self.assertEqual(mock_update_setting_with_rollback.call_count, 4)
             self.assertEqual(mock_firmware_upgrade_with_rollback.call_count, 1)
