f6442dc473186777fa65c5edf6c2fa030017ecfd
Pritam Ghanghas <pghanghas@nebra.com>
2022-03-28 19:37:02 +0530

    feat: Light miner: Diagnostics page should NOT display blockchain details
    * use grpc client for diagnostic information
    * remove p2p and local sync related information

--- hw_diag/tests/test_get_miner_diag.py
+++ hw_diag/tests/test_get_miner_diag.py
@@ -1,78 +1,45 @@
 import unittest
-
 from unittest.mock import patch
-from copy import deepcopy
+import grpc
+from concurrent import futures
 
-import hm_pyhelper.miner_json_rpc.client
+import hm_pyhelper
+from hm_pyhelper.protos import local_pb2_grpc
+from hm_pyhelper.tests.test_gateway_grpc import TestData, MockServicer
 
 from hw_diag.utilities.miner import fetch_miner_data
 
 
 class TestGetMinerDiag(unittest.TestCase):
-    PEER_ADDR = {
-        'peer_addr':
-            '/p2p/11rMJrFystAT3mLEdgeeVo2opSmHuMb2RXFVYh7qfqhqt2GkPv9'
-    }
 
-    HEIGHT = {'height': 1045324}
+    def start_mock_server(self):
+        self.mock_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        local_pb2_grpc.add_apiServicer_to_server(MockServicer(), self.mock_server)
+        self.mock_server.add_insecure_port(f'[::]:{TestData.server_port}')
+        self.mock_server.start()
 
-    PEER_BOOK = [
-        {
-            'connection_count': 5,
-            'listen_addr_count': 1,
-            'name': 'wild-purple-tuna',
-            'nat': 'static'
-        }
-    ]
+    @patch('hm_pyhelper.gateway_grpc.client.GatewayClient.get_gateway_version',
+           return_value=TestData.expected_summary['gateway_version'])
+    def test_fetch_miner_data_valid(self, mock_get_gateway_version):
+        with patch.object(hm_pyhelper.gateway_grpc.client.GatewayClient.__init__,
+                          '__defaults__', (f"localhost:{TestData.server_port}",)):
+            self.start_mock_server()
+            expected_data = {
+                'validator_address': TestData.validator_address_decoded,
+                'validator_uri': TestData.height_res.gateway.uri,
+                'block_age': TestData.height_res.block_age,
+                'MH': TestData.height_res.height,
+                'RE': TestData.region_name,
+                'miner_key': TestData.pubkey_decoded,
+                'FW': TestData.expected_summary['gateway_version']
+            }
 
-    @patch.object(hm_pyhelper.miner_json_rpc.client.Client, 'get_peer_addr', return_value=PEER_ADDR)
-    @patch.object(hm_pyhelper.miner_json_rpc.client.Client, 'get_peer_book',
-                  return_value=deepcopy(PEER_BOOK))
-    @patch.object(hm_pyhelper.miner_json_rpc.client.Client, 'get_height', return_value=HEIGHT)
-    def test_fetch_miner_data_valid(self, mock_height, mock_book, mock_addr):
-        expected_data = {
-            'MC': True,
-            'MD': True,
-            'MH': 1045324,
-            'MN': 'static',
-            'MR': False
-        }
+            result = fetch_miner_data({})
+            print(result)
+            self.assertEqual(result, expected_data)
+            self.mock_server.stop(grace=0)
 
-        result = fetch_miner_data({})
-        self.assertEqual(result, expected_data)
-
-    @patch.object(hm_pyhelper.miner_json_rpc.client.Client, 'get_peer_addr', return_value=PEER_ADDR)
-    @patch.object(hm_pyhelper.miner_json_rpc.client.Client, 'get_peer_book',
-                  return_value=deepcopy(PEER_BOOK))
-    @patch.object(hm_pyhelper.miner_json_rpc.client.Client, 'get_height', return_value=HEIGHT)
-    def test_fetch_miner_data_relayed(self, mock_height, mock_book, mock_addr):
-        mock_book.return_value[0]['nat'] = 'symmetric'
-
-        expected_data = {
-            'MC': True,
-            'MD': True,
-            'MH': 1045324,
-            'MN': 'symmetric',
-            'MR': True
-        }
-
-        result = fetch_miner_data({})
-        self.assertEqual(result, expected_data)
-
-    @patch.object(hm_pyhelper.miner_json_rpc.client.Client, 'get_peer_addr', return_value=PEER_ADDR)
-    @patch.object(hm_pyhelper.miner_json_rpc.client.Client, 'get_peer_book',
-                  return_value=deepcopy(PEER_BOOK))
-    @patch.object(hm_pyhelper.miner_json_rpc.client.Client, 'get_height', return_value=HEIGHT)
-    def test_fetch_miner_data_not_connected(self, mock_height, mock_book, mock_addr):
-        mock_book.return_value[0]['connection_count'] = 0
-
-        expected_data = {
-            'MC': False,
-            'MD': True,
-            'MH': 1045324,
-            'MN': 'static',
-            'MR': False
-        }
-
-        result = fetch_miner_data({})
-        self.assertEqual(result, expected_data)
+    def test_fetch_miner_data_not_connected(self):
+        diagnostic_data = {}
+        fetch_miner_data(diagnostic_data)
+        self.assertEqual(diagnostic_data, {})
