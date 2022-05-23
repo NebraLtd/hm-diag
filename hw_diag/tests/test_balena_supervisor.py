27bc3b26085f2ecdf582ea241a0c3ac9aa303602
Zhang <zpoterasu@nebra.com>
2022-05-23 20:19:55 +0800

    feat: add utility method for Balena supervisor reboot device API

--- hw_diag/tests/test_balena_supervisor.py
+++ hw_diag/tests/test_balena_supervisor.py
@@ -13,6 +13,7 @@ TEST_SUPERVISOR_ADDRESS = 'http://127.0.0.1'
 TEST_SUPERVISOR_API_KEY = 'secret'
 TEST_SUPERVISOR_DEVICE_STATUS_URL = 'http://127.0.0.1/v2/state/status?apikey=secret'
 TEST_SUPERVISOR_SHUTDOWN_URL = 'http://127.0.0.1/v1/shutdown?apikey=secret'
+TEST_SUPERVISOR_REBOOT_URL = 'http://127.0.0.1/v1/reboot?apikey=secret'
 
 
 class TestBalenaSupervisor(unittest.TestCase):
@@ -156,3 +157,63 @@ class TestBalenaSupervisor(unittest.TestCase):
             bs.shutdown()
 
         assert str(exp.exception) == 'shutdown failed due to supervisor API issue'
+
+    @responses.activate
+    def test_reboot_gateway_success_response(self):
+        responses.add(
+            responses.POST,
+            TEST_SUPERVISOR_REBOOT_URL,
+            status=200,
+            json={"Data": "OK", "Error": ""}
+        )
+
+        bs = BalenaSupervisor(TEST_SUPERVISOR_ADDRESS, TEST_SUPERVISOR_API_KEY)
+        resp = bs.reboot()
+
+        assert resp['Data'] == 'OK'
+
+    @responses.activate
+    def test_reboot_gateway_error_on_connection_error(self):
+        responses.add(
+            responses.POST,
+            TEST_SUPERVISOR_REBOOT_URL,
+            body=ConnectionError('Unable to connect')
+        )
+
+        bs = BalenaSupervisor(TEST_SUPERVISOR_ADDRESS, TEST_SUPERVISOR_API_KEY)
+
+        with self.assertRaises(Exception) as exp:
+            bs.reboot()
+
+        assert str(exp.exception) == "supervisor API not accessible"
+
+    @responses.activate
+    def test_reboot_gateway_error_on_connection_timeout(self):
+        responses.add(
+            responses.POST,
+            TEST_SUPERVISOR_REBOOT_URL,
+            body=ConnectTimeout('Timout trying to make connection')
+        )
+
+        bs = BalenaSupervisor(TEST_SUPERVISOR_ADDRESS, TEST_SUPERVISOR_API_KEY)
+
+        with self.assertRaises(Exception) as exp:
+            bs.reboot()
+
+        assert str(exp.exception) == 'supervisor API not accessible'
+
+    @responses.activate
+    def test_reboot_gateway_empty_response(self):
+        responses.add(
+            responses.POST,
+            TEST_SUPERVISOR_REBOOT_URL,
+            body='',
+            status=200
+        )
+
+        bs = BalenaSupervisor(TEST_SUPERVISOR_ADDRESS, TEST_SUPERVISOR_API_KEY)
+
+        with self.assertRaises(Exception) as exp:
+            bs.reboot()
+
+        assert str(exp.exception) == 'reboot failed due to supervisor API issue'
