d20fa8ce343fa3cdb9671b2ee902bf7ab16b215e
Zhang <zpoterasu@nebra.com>
2022-05-30 13:58:13 +0800

    feat: get network connectivity using dbus interface of network manager

--- hw_diag/utilities/dbus_proxy/dbus_ids.py
+++ hw_diag/utilities/dbus_proxy/dbus_ids.py
@@ -1,10 +1,11 @@
 
 class DBusIds:
-    '''DBus Identities'''
-
+    """DBus Identities"""
+    # Object manager
     DBUS_PROPERTIES_IF = 'org.freedesktop.DBus.Properties'
     DBUS_OBJECTMANAGER_IF = 'org.freedesktop.DBus.ObjectManager'
 
+    # Modem manager
     DBUS_MM1_SERVICE = 'org.freedesktop.ModemManager1'
     DBUS_MM1_PATH = '/org/freedesktop/ModemManager1'
     DBUS_MM1_IF = 'org.freedesktop.ModemManager1'
@@ -14,9 +15,17 @@ class DBusIds:
     DBUS_MM1_MODEM_3GPP_IF = 'org.freedesktop.ModemManager1.Modem.Modem3gpp'
     DBUS_MM1_MODEM_CDMA_IF = 'org.freedesktop.ModemManager1.Modem.ModemCdma'
 
+    # Network manager
+    DBUS_NM_SERVICE = 'org.freedesktop.NetworkManager'
+    DBUS_NM_PATH = '/org/freedesktop/NetworkManager'
+    DBUS_NM_IF = 'org.freedesktop.NetworkManager'
+
+    # Systemd
     SYSTEMD_SERVICE = 'org.freedesktop.systemd1'
     SYSTEMD_PATH = '/org/freedesktop/systemd1'
     SYSTEMD_MANAGER_IF = 'org.freedesktop.systemd1.Manager'
     SYSTEMD_UNIT_IF = 'org.freedesktop.systemd1.Unit'
 
+    # Systemd units
     MODEM_MANAGER_UNIT_NAME = 'ModemManager.service'
+    NETWORK_MANAGER_UNIT_NAME = 'NetworkManager.service'
