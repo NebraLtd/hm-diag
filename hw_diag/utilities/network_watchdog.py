5ac48c6edb4030d38301347b42ffdc2b09d68721
Zhang <zpoterasu@nebra.com>
2022-06-01 03:58:04 +0800

    feat: save last restart time using KeyStore

--- hw_diag/utilities/network_watchdog.py
+++ hw_diag/utilities/network_watchdog.py
@@ -9,10 +9,17 @@ from hw_diag.utilities.balena_supervisor import BalenaSupervisor
 from hw_diag.utilities.dbus_proxy.dbus_ids import DBusIds
 from hw_diag.utilities.dbus_proxy.network_manager import NetworkManager
 from hw_diag.utilities.dbus_proxy.systemd import Systemd
+from hw_diag.utilities.keystore import KeyStore
 
-WATCHDOG_LOG_FILE_NAME = '/var/data/watchdog.log'
-LAST_RESTART_FILE_NAME = '/var/data/last_restart.txt'
+if os.access('/var/watchdog', os.W_OK):
+    WATCHDOG_LOG_FILE_PATH = '/var/watchdog/watchdog.log'
+    LAST_RESTART_FILE_PATH = '/var/watchdog/last_restart.json'
+else:
+    WATCHDOG_LOG_FILE_PATH = '/tmp/watchdog.log'
+    LAST_RESTART_FILE_PATH = '/tmp/last_restart.json'
+
 MAX_LOG_SIZE = 10 * 1024 * 1024                                             # 10 Mb
+LAST_RESTART_KEY = 'last_restart'
 LAST_RESTART_DATE_FORMAT = '%d/%m/%Y %H:%M:%S'
 
 # Full system reboot limited to once a day
@@ -28,7 +35,7 @@ PUBLIC_DNS_PORT = 53
 
 # Add rotating log handler to the logger
 logger = get_logger(__name__)
-handler = RotatingFileHandler(WATCHDOG_LOG_FILE_NAME, maxBytes=MAX_LOG_SIZE, backupCount=3)
+handler = RotatingFileHandler(WATCHDOG_LOG_FILE_PATH, maxBytes=MAX_LOG_SIZE, backupCount=3)
 handler.setLevel(logging.INFO)
 logger.addHandler(handler)
 
@@ -42,7 +49,7 @@ class NetworkWatchdog:
         self.network_manager_unit = self.systemd_proxy.get_unit(DBusIds.NETWORK_MANAGER_UNIT_NAME)
         self.network_manager = NetworkManager()
 
-    def _have_internet(self) -> bool:
+    def have_internet(self) -> bool:
         try:
             sock = socket.create_connection((PUBLIC_DNS_SERVER, PUBLIC_DNS_PORT))
             if sock:
@@ -57,7 +64,7 @@ class NetworkWatchdog:
 
         # Log more details about the network connectivity and internet connectivity
         logger.info(f"Network manager connectivity: {self.network_manager.get_connect_state()}")
-        logger.info(f"Internet connectivity: {self._have_internet()}")
+        logger.info(f"Internet connectivity: {self.have_internet()}")
 
         nm_connected = self.network_manager.is_connected()
         return nm_connected
@@ -68,25 +75,16 @@ class NetworkWatchdog:
         logger.info(f"modem manager restarted: {mm_restarted}")
 
     def get_last_restart(self) -> datetime:
-        last_restart_file = None
         try:
-            last_restart_file = open(LAST_RESTART_FILE_NAME)
-            return datetime.strptime(last_restart_file.read(), LAST_RESTART_DATE_FORMAT)
+            last_restart = KeyStore(LAST_RESTART_FILE_PATH).get(LAST_RESTART_KEY)
+            return datetime.strptime(last_restart, LAST_RESTART_DATE_FORMAT)
         except Exception as e:
             logger.info(f"Can not find the previous restart time: {e}")
             return datetime.min
-        finally:
-            try:
-                if last_restart_file:
-                    last_restart_file.close()
-            except Exception as e:
-                logger.info(f"Can not close the file: {e}")
-                pass
 
     def save_last_restart(self) -> None:
-        with open(LAST_RESTART_FILE_NAME, 'w') as last_restart_file:
-            last_restart_file.write("\n" + datetime.now().strftime(LAST_RESTART_DATE_FORMAT))
-            last_restart_file.close()
+        KeyStore(LAST_RESTART_FILE_PATH).set(LAST_RESTART_KEY,
+                                             datetime.now().strftime(LAST_RESTART_DATE_FORMAT))
 
     def run_watchdog(self) -> None:
         logger.info("Running the watchdog...")
