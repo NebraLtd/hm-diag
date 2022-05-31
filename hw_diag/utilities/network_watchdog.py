ddf59dfc8f467c93740f6928a27757f3fe0ef1e0
Zhang <zpoterasu@nebra.com>
2022-05-31 10:02:51 +0800

    feat: use sized based log rotating for watchdog log

--- hw_diag/utilities/network_watchdog.py
+++ hw_diag/utilities/network_watchdog.py
@@ -1,16 +1,27 @@
+import logging
 import os
 from datetime import datetime, timedelta
+from logging.handlers import RotatingFileHandler
+
 from hm_pyhelper.logger import get_logger
 from hw_diag.utilities.balena_supervisor import BalenaSupervisor
 from hw_diag.utilities.dbus_proxy.dbus_ids import DBusIds
 from hw_diag.utilities.dbus_proxy.network_manager import NetworkManager
 from hw_diag.utilities.dbus_proxy.systemd import Systemd
 
-logging = get_logger(__name__)
+logger = get_logger(__name__)
+
+WATCHDOG_LOG_FILE_NAME = '/var/data/watchdog.log'
+LAST_RESTART_FILE_NAME = '/var/data/last_restart.txt'
+MAX_LOG_SIZE = 10 * 1024 * 1024                                             # 10 Mb
+
+# Add rotating log handler to the logger
+handler = RotatingFileHandler(WATCHDOG_LOG_FILE_NAME, maxBytes=MAX_LOG_SIZE, backupCount=3)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 
 class NetworkWatchdog:
-    LAST_RESTART_FILE_NAME = '/var/data/last_restart.txt'
     LAST_RESTART_DATE_FORMAT = '%d/%m/%Y %H:%M:%S'
 
     # Full system reboot limited to once a day
@@ -33,7 +44,7 @@ class NetworkWatchdog:
     def restart_network_manager(self):
         """Restart hostOS NetworkManager service"""
         mm_restarted = self.network_manager_unit.wait_restart()
-        logging.info(f"modem manager restarted: {mm_restarted}")
+        logger.info(f"modem manager restarted: {mm_restarted}")
 
     def get_last_restart(self) -> datetime:
         last_restart_file = None
@@ -41,14 +52,14 @@ class NetworkWatchdog:
             last_restart_file = open(self.LAST_RESTART_FILE_NAME)
             return datetime.strptime(last_restart_file.read(), self.LAST_RESTART_DATE_FORMAT)
         except Exception as e:
-            logging.info(f"Can not find the previous restart time: {e}")
+            logger.info(f"Can not find the previous restart time: {e}")
             return datetime.min
         finally:
             try:
                 if last_restart_file:
                     last_restart_file.close()
             except Exception as e:
-                logging.info(f"Can not close the file: {e}")
+                logger.info(f"Can not close the file: {e}")
                 pass
 
     def save_last_restart(self) -> None:
@@ -57,37 +68,37 @@ class NetworkWatchdog:
             last_restart_file.close()
 
     def check_network_connectivity(self) -> None:
-        logging.info("Checking the network connectivity.")
+        logger.info("Checking the network connectivity.")
 
         if self.network_manager.is_connected():
             self.lost_count = 0
-            logging.info("Internet is working.")
+            logger.info("Internet is working.")
         else:
             self.lost_count += 1
-            logging.warning(f"Network is not connected! Lost connectivity count={self.lost_count}")
+            logger.warning(f"Network is not connected! Lost connectivity count={self.lost_count}")
 
             if self.lost_count > self.NM_RESTART_THRESHOLD:
-                logging.warning(
+                logger.warning(
                     "Reached out to the lost connectivity count to restart the network manager.")
                 self.restart_network_manager()
-                logging.info("Restarted the network connection.")
+                logger.info("Restarted the network connection.")
 
                 if self.network_manager.is_connected():
                     self.lost_count = 0
-                    logging.info("Internet is working after restarting the network connection.")
+                    logger.info("Internet is working after restarting the network connection.")
                 else:
-                    logging.warning("Internet is still not working.")
+                    logger.warning("Internet is still not working.")
 
                     if self.lost_count > self.FULL_REBOOT_THRESHOLD:
-                        logging.warning(
+                        logger.warning(
                             "Reached out to the lost connectivity count to reboot the hotspot.")
                         if datetime.now() - timedelta(
                                 hours=self.REBOOT_LIMIT_HOURS) < self.get_last_restart():
-                            logging.info(
+                            logger.info(
                                 "Hotspot has already been restarted within a day, skipping.")
                         else:
                             self.save_last_restart()
-                            logging.info("Rebooting the device.")
+                            logger.info("Rebooting the device.")
 
                             balena_supervisor = BalenaSupervisor.new_from_env()
                             balena_supervisor.reboot(force=True)
