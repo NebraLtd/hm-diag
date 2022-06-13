81c2bbf91d2f7d44de28c35144ec8d1c55d590ab
Zhang <zpoterasu@nebra.com>
2022-06-13 13:47:28 +0800

    feat: ping local gateway using ICMP

--- hw_diag/tests/test_network_watchdog.py
+++ hw_diag/tests/test_network_watchdog.py
@@ -1,11 +1,82 @@
 import unittest
 from unittest.mock import patch
 
+from icmplib import Hop, Host
+
 import hw_diag
 from hw_diag.utilities.network_watchdog import NetworkWatchdog
 
 
 class TestNetworkWatchdog(unittest.TestCase):
+    TEST_INTERNAL_NETWORK_IP = '10.16.18.1'     # NOSONAR
+    TEST_GATEWAY_IP = '192.168.1.1'             # NOSONAR
+
+    @patch('hw_diag.utilities.network_watchdog.traceroute',
+           return_value=[Hop(address=TEST_INTERNAL_NETWORK_IP,
+                             packets_sent=2,
+                             rtts=[0.4, 0.3],
+                             distance=1),
+                         Hop(address=TEST_GATEWAY_IP,
+                             packets_sent=2,
+                             rtts=[0.4, 0.3],
+                             distance=2)])
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
+    def test_find_gateway_ip_success(self, _, __, ___):
+        watchdog = NetworkWatchdog.get_instance()
+        gateway_ip = watchdog.find_gateway_ip()
+        self.assertEqual(gateway_ip, self.TEST_GATEWAY_IP)
+
+    @patch('hw_diag.utilities.network_watchdog.traceroute', return_value=[])
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
+    def test_find_gateway_ip_fail(self, _, __, ___):
+        watchdog = NetworkWatchdog.get_instance()
+        gateway_ip = watchdog.find_gateway_ip()
+        self.assertEqual(gateway_ip, '')
+
+    @patch.object(hw_diag.utilities.keystore.KeyStore, 'set')
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
+    def test_save_previous_gateway_ip_success(self, _, __, ___):
+        watchdog = NetworkWatchdog.get_instance()
+        watchdog.save_previous_gateway_ip(self.TEST_GATEWAY_IP)
+
+    @patch.object(hw_diag.utilities.keystore.KeyStore, 'get', return_value=TEST_GATEWAY_IP)
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
+    def test_get_previous_gateway_ip_success(self, _, __, ___):
+        watchdog = NetworkWatchdog.get_instance()
+        previous_gateway_ip = watchdog.get_previous_gateway_ip()
+        self.assertEqual(previous_gateway_ip, self.TEST_GATEWAY_IP)
+
+    @patch.object(hw_diag.utilities.keystore.KeyStore, 'get', return_value='')
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
+    def test_get_previous_gateway_ip_fail(self, _, __, ___):
+        watchdog = NetworkWatchdog.get_instance()
+        previous_gateway_ip = watchdog.get_previous_gateway_ip()
+        self.assertEqual(previous_gateway_ip, '')
+
+    @patch('hw_diag.utilities.network_watchdog.ping', return_value=Host(address=TEST_GATEWAY_IP,
+                                                                        packets_sent=4,
+                                                                        rtts=[0.3, 0.4]))
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
+    def test_icmp_ping_success(self, _, __, ___):
+        watchdog = NetworkWatchdog.get_instance()
+        result = watchdog.icmp_ping(self.TEST_GATEWAY_IP)
+        self.assertTrue(result)
+
+    @patch('hw_diag.utilities.network_watchdog.ping', return_value=Host(address=TEST_GATEWAY_IP,
+                                                                        packets_sent=0,
+                                                                        rtts=[]))
+    @patch("dbus.SystemBus")
+    @patch("dbus.Interface")
+    def test_icmp_ping_fail(self, _, __, ___):
+        watchdog = NetworkWatchdog.get_instance()
+        result = watchdog.icmp_ping('')
+        self.assertFalse(result)
 
     @patch('socket.create_connection')
     @patch("dbus.SystemBus")
@@ -23,22 +94,24 @@ class TestNetworkWatchdog(unittest.TestCase):
         have_internet = watchdog.have_internet()
         self.assertFalse(have_internet)
 
-    @patch('socket.create_connection')
-    @patch.object(hw_diag.utilities.dbus_proxy.network_manager.NetworkManager, 'is_connected',
+    @patch.object(hw_diag.utilities.network_watchdog.NetworkWatchdog, 'icmp_ping',
                   return_value=True)
+    @patch.object(hw_diag.utilities.network_watchdog.NetworkWatchdog, 'find_gateway_ip')
+    @patch('socket.create_connection')
     @patch("dbus.SystemBus")
     @patch("dbus.Interface")
-    def test_is_connected(self, _, __, ___, ____):
+    def test_is_connected(self, _, __, ___, ____, _____):
         watchdog = NetworkWatchdog.get_instance()
         is_connected = watchdog.is_connected()
         self.assertTrue(is_connected)
 
-    @patch('socket.create_connection', side_effect=TimeoutError())
-    @patch.object(hw_diag.utilities.dbus_proxy.network_manager.NetworkManager, 'is_connected',
+    @patch.object(hw_diag.utilities.network_watchdog.NetworkWatchdog, 'icmp_ping',
                   return_value=False)
+    @patch.object(hw_diag.utilities.network_watchdog.NetworkWatchdog, 'find_gateway_ip')
+    @patch('socket.create_connection')
     @patch("dbus.SystemBus")
     @patch("dbus.Interface")
-    def test_not_connected(self, _, __, ___, ____):
+    def test_not_connected(self, _, __, ___, ____, _____):
         watchdog = NetworkWatchdog.get_instance()
         is_connected = watchdog.is_connected()
         self.assertFalse(is_connected)
@@ -55,22 +128,21 @@ class TestNetworkWatchdog(unittest.TestCase):
     @patch.object(NetworkWatchdog, 'is_connected', return_value=True)
     @patch("dbus.SystemBus")
     @patch("dbus.Interface")
-    def test_run_watchdog_connected(self, _, __, ___):
+    def test_ensure_network_connection_connected(self, _, __, ___):
         watchdog = NetworkWatchdog.get_instance()
         with self.assertLogs(level='INFO') as captured_logs:
             watchdog.ensure_network_connection()
-            self.assertIn('INFO:hw_diag.utilities.network_watchdog:Running the watchdog...',
-                          captured_logs.output)
-            self.assertIn('INFO:hw_diag.utilities.network_watchdog:Network is working.',
-                          captured_logs.output)
+            self.assertIn('Ensuring the network connection...', captured_logs.output[0])
+            self.assertIn('Network is working.', captured_logs.output[1])
 
     @patch.object(NetworkWatchdog, 'is_connected', return_value=False)
+    @patch.object(hw_diag.utilities.network_watchdog.NetworkWatchdog, 'restart_network_manager')
     @patch("dbus.SystemBus")
     @patch("dbus.Interface")
-    def test_run_watchdog_disconnected(self, _, __, ___):
+    def test_ensure_network_connection_disconnected(self, _, __, ___, ____):
         watchdog = NetworkWatchdog.get_instance()
         with self.assertLogs(level='INFO') as captured_logs:
             watchdog.ensure_network_connection()
-            self.assertIn('Running the watchdog...', captured_logs.output[0])
+            self.assertIn('Ensuring the network connection...', captured_logs.output[0])
             self.assertIn(
                 'Network is not connected! Lost connectivity count=1', captured_logs.output[1])
--- hw_diag/utilities/network_watchdog.py
+++ hw_diag/utilities/network_watchdog.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import logging
 import os
 import tempfile
@@ -6,10 +8,13 @@ from logging.handlers import RotatingFileHandler
 import socket
 
 from hm_pyhelper.logger import get_logger
+from icmplib import traceroute, ping
+
 from hw_diag.utilities.balena_supervisor import BalenaSupervisor
 from hw_diag.utilities.dbus_proxy.dbus_ids import DBusIds
 from hw_diag.utilities.dbus_proxy.network_manager import NetworkManager
 from hw_diag.utilities.dbus_proxy.systemd import Systemd
+from hw_diag.utilities.keystore import KeyStore
 
 LOGLEVEL = os.environ.get("LOGLEVEL", "DEBUG")
 _log_format = "%(asctime)s - [%(levelname)s] - (%(filename)s:%(lineno)d) - %(message)s"
@@ -20,11 +25,10 @@ class NetworkWatchdog:
 
     VOLUME_PATH = '/var/watchdog/'
     WATCHDOG_LOG_FILE_NAME = 'watchdog.log'
-    LAST_RESTART_FILE_NAME = 'last_restart.json'
-
     MAX_LOG_SIZE = 10 * 1024 * 1024  # 10 Mb
-    LAST_RESTART_KEY = 'last_restart'
-    LAST_RESTART_DATE_FORMAT = '%d/%m/%Y %H:%M:%S'
+
+    WATCHDOG_STATE_FILE_NAME = 'state.json'
+    PREVIOUS_GATEWAY_KEY = 'previous_gateway'
 
     # Failed connectivity count for the network manager to restart
     NM_RESTART_THRESHOLD = int(os.environ.get("NM_RESTART_THRESHOLD", 1))
@@ -49,7 +53,7 @@ class NetworkWatchdog:
     reboot_request_count = 0
 
     @staticmethod
-    def get_instance():
+    def get_instance() -> NetworkWatchdog:
         """ Static method to fetch the current instance.
         """
         if not NetworkWatchdog._instance:
@@ -70,11 +74,11 @@ class NetworkWatchdog:
         # Prepare the log file location
         if os.access(self.VOLUME_PATH, os.W_OK):
             self.log_file_path = os.path.join(self.VOLUME_PATH, self.WATCHDOG_LOG_FILE_NAME)
-            self.state_file_path = os.path.join(self.VOLUME_PATH, self.LAST_RESTART_FILE_NAME)
+            self.state_file_path = os.path.join(self.VOLUME_PATH, self.WATCHDOG_STATE_FILE_NAME)
         else:
             self.temp_dir = tempfile.TemporaryDirectory()
             self.log_file_path = os.path.join(self.temp_dir.name, self.WATCHDOG_LOG_FILE_NAME)
-            self.state_file_path = os.path.join(self.temp_dir.name, self.LAST_RESTART_FILE_NAME)
+            self.state_file_path = os.path.join(self.temp_dir.name, self.WATCHDOG_STATE_FILE_NAME)
 
         # Set up logger
         self.LOGGER = get_logger(__name__)
@@ -95,32 +99,70 @@ class NetworkWatchdog:
         if hasattr(self, 'temp_dir'):
             self.temp_dir.cleanup()
 
+    def find_gateway_ip(self) -> str:
+        hops = traceroute(self.PUBLIC_DNS_SERVER)
+
+        """
+        The local gateway IP should be the address of the 2nd hop.
+        Also first hop and 2nd hop should have the different addresses,
+        otherwise 2nd hop address is invalid address.
+        """
+        if len(hops) >= 2 and hops[0].address != hops[1].address and hops[1].is_alive:
+            gateway_ip = hops[1].address
+            self.LOGGER.info(f"Found local gateway {gateway_ip} using traceroute.")
+            self.save_previous_gateway_ip(gateway_ip)
+        else:
+            self.LOGGER.info("Failed to find local gateway using traceroute.")
+            gateway_ip = self.get_previous_gateway_ip()
+            self.LOGGER.info(f"Previous local gateway: {gateway_ip}")
+
+        return gateway_ip
+
+    def save_previous_gateway_ip(self, ip: str) -> None:
+        store = KeyStore(self.state_file_path)
+        store.set(self.PREVIOUS_GATEWAY_KEY, ip)
+
+    def get_previous_gateway_ip(self) -> str:
+        try:
+            return KeyStore(self.state_file_path).get(self.PREVIOUS_GATEWAY_KEY, '')
+        except Exception:
+            return ''
+
+    def icmp_ping(self, ip: str) -> bool:
+        try:
+            result = ping(ip)
+            return result and result.is_alive
+        except Exception:
+            return False
+
     def have_internet(self, timeout=10) -> bool:
         try:
             socket.setdefaulttimeout(timeout)
             sock = socket.create_connection((self.PUBLIC_DNS_SERVER, self.PUBLIC_DNS_PORT))
             sock.close()
             return True
-        except Exception as e:
-            self.LOGGER.info(f"internet not accessible: {e}")
+        except Exception:
             return False
 
     def is_connected(self) -> bool:
-        self.LOGGER.info("Checking the network connectivity.")
+        local_gateway_connectivity = False
+        gateway_ip = self.find_gateway_ip()
+        if gateway_ip:
+            local_gateway_connectivity = self.icmp_ping(gateway_ip)
 
-        # Log more details about the network connectivity and internet connectivity
-        self.LOGGER.info(f"Network manager state: {self.network_manager.get_connect_state()}")
-        self.LOGGER.info(f"Internet connectivity: {self.have_internet()}")
+        # Log more details about network connection status
+        self.LOGGER.info(f"Local gateway({gateway_ip}) connectivity: {local_gateway_connectivity}")
+        self.LOGGER.info(f"Internet({self.PUBLIC_DNS_SERVER}) connectivity: {self.have_internet()}")
 
-        return self.have_internet()
+        return local_gateway_connectivity
 
-    def restart_network_manager(self):
+    def restart_network_manager(self) -> None:
         """Restart hostOS NetworkManager service"""
         nm_restarted = self.network_manager_unit.wait_restart()
         self.LOGGER.info(f"Network manager restarted: {nm_restarted}")
 
     def ensure_network_connection(self) -> None:
-        self.LOGGER.info("Running the watchdog...")
+        self.LOGGER.info("Ensuring the network connection...")
 
         # If network is connected, nothing to do more
         if self.is_connected():
@@ -161,7 +203,5 @@ class NetworkWatchdog:
 
 
 if __name__ == '__main__':
-    print('Watchdog is running...')
     watchdog = NetworkWatchdog()
-    is_connected = watchdog.is_connected()
-    print("Is connected:", is_connected)
+    watchdog.ensure_network_connection()
