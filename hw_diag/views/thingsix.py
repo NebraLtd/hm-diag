cb9f6556f3966369eca14b3d4793dc2fb65361b0
Robert Putt <robe8437@C02FV3BTMD6R.local>
2023-03-13 19:20:55 +0000

    Render THIX page

--- hw_diag/views/thingsix.py
+++ hw_diag/views/thingsix.py
@@ -2,10 +2,13 @@ import logging
 import os
 
 from flask import Blueprint
+from flask import render_template
 
 from hm_pyhelper.logger import get_logger
 from hw_diag.utilities.auth import authenticate
 from hw_diag.utilities.auth import commercial_fleet_only
+from hw_diag.utilities.db import get_value
+from hw_diag.utilities.db import set_value
 
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG"))
@@ -18,4 +21,19 @@ THINGSIX = Blueprint('THINGSIX', __name__)
 @authenticate
 @commercial_fleet_only
 def get_thix_dashboard():
-    return 'Hello', 200
+    # If THIX isn't enabled render the setup page.
+    try:
+        if not bool(get_value('thix_enabled')):
+            render_template('thix_setup.html')
+    except Exception:
+        return render_template('thix_setup.html')
+
+    # If THIX isn't onboarded render the onboard page.
+    try:
+        if not bool(get_value('thix_onboarded')):
+            render_template('thix_onboard.html')
+    except Exception:
+        return render_template('thix_onboard.html')
+
+    # Else just render the THIX dashboard.
+    return render_template('thix_dashboard.html')
