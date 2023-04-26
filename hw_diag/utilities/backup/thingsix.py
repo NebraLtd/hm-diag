38b5dd80a86de3b3a667634f5d33874771e99581
Robert Putt <robe8437@C02FV3BTMD6R.local>
2023-04-26 18:54:52 +0100

    Add backup & restore feature

---
+++ hw_diag/utilities/backup/thingsix.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+from hw_diag.utilities.backup.base import BaseBackupRestore
+
+
+THIX_DIR = '/var/thix'
+
+
+class ThingsIXBackupRestore(BaseBackupRestore):
+    name = 'THINGSIX'
+
+    def backup(self):
+        shutil.copytree(THIX_DIR, self.tmpdir)
+
+    def restore(self):
+        os.system('cp -r %s/* %s/.' % (self.tmpdir, THIX_DIR))
