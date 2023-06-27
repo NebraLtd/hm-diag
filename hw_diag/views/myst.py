bdfd616bdeb2c7105ef9fe211ef6ab1ca0c20505
Aaron Shaw <shawaj@gmail.com>
2023-06-27 19:57:33 +0100

    Update myst.py

--- hw_diag/views/myst.py
+++ hw_diag/views/myst.py
@@ -9,6 +9,7 @@ from hm_pyhelper.logger import get_logger
 from hw_diag.utilities.auth import authenticate
 from hw_diag.utilities.auth import commercial_fleet_only
 from hw_diag.utilities.diagnostics import read_diagnostics_file
+from hw_diag.utilities.dashboard_registration import claim_miner_deeplink
 
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG"))
@@ -22,10 +23,12 @@ MYST = Blueprint('MYST', __name__)
 @commercial_fleet_only
 def get_myst_dashboard():
     diagnostics = read_diagnostics_file()
+    claim_deeplink = claim_miner_deeplink()
     now = round(time.time())
     
     return render_template(
         'myst.html',
         diagnostics=diagnostics,
+        claim_deeplink=claim_deeplink,
         now=now
     )
