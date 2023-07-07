cf37e4f53094dbcb12279666e997250a55def4a0
Aaron Shaw <shawaj@gmail.com>
2023-07-07 01:52:19 +0100

    Add versions

--- hw_diag/utilities/shell.py
+++ hw_diag/utilities/shell.py
@@ -11,9 +11,22 @@ def get_environment_var(diagnostics):
         'FREQ',
         'FIRMWARE_VERSION',
         'VARIANT',
-        'FIRMWARE_SHORT_HASH'
+        'FIRMWARE_SHORT_HASH',
+        'DIAGNOSTICS_VERSION',
+        'CONFIG_VERSION',
+        'PKTFWD_VERSION',
+        'GATEWAYRS_VERSION',
+        'MULTIPLEXER_VERSION'
     ]
-    keys = ["BN", "ID", "BA", "FR", "FW", "VA", "firmware_short_hash"]
+    keys = ["BN", "ID", "BA", "FR", "FW", "VA", "firmware_short_hash", "diagnotics_version",
+            "config_version", "packet_forwarder_version", "gatewayrs_version", "multiplexer_version"
+    ]
 
     for (var, key) in zip(env_var, keys):
         diagnostics[key] = os.getenv(var)
+
+    if "MYST_VERSION" in os.environ:
+        diagnostics["myst_version"] = os.getenv("MYST_VERSION")
+
+    if "THINGSIX_VERSION" in os.environ:
+        diagnostics["thingsix_version"] = os.getenv("THINGSIX_VERSION")
